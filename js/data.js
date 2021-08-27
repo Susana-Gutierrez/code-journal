306ec8ac8c4a1edfbe0c9c24f5a9d55dde22a396
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2021-08-27 16:52:11 -0700

    Completed view-their-entries.

--- js/data.js
+++ js/data.js
@@ -1,16 +1,11 @@
 /* exported data */
 
-var data = [];
-
-const $form = document.forms[0];
-const $title = document.forms[0].elements[0];
-const $PhotoURL = document.forms[0].elements[1];
-const $notes = document.forms[0].elements[2];
-
-const $entryForm = document.querySelector('.entry-journal');
-const $entries = document.querySelector('.entries-journal');
-const $noEntry = document.querySelector('.no-entry-message');
-const $imageElement = document.querySelector('img');
+var data = {
+  view: 'entry-form',
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
 
 const previouscodeJournalJSON = localStorage.getItem('javascript-local-storage');
 
@@ -18,85 +13,9 @@ if (previouscodeJournalJSON !== null) {
   data = JSON.parse(previouscodeJournalJSON);
 }
 
-if (data.length !== 0) {
-  $noEntry.className = 'hidden';
-}
-
-function newEntryDOM(jornalElement) {
-
-  const elementDiv = document.createElement('div');
-  elementDiv.setAttribute('class', 'row');
-  const elementDiv2 = document.createElement('div');
-  elementDiv2.setAttribute('class', 'column-full column-half');
-  const elementImg = document.createElement('img');
-
-  const elementDiv3 = document.createElement('div');
-  elementDiv3.setAttribute('class', 'column-half');
-  const elementLi = document.createElement('li');
-  const elementH2 = document.createElement('h2');
-
-  const elementLi2 = document.createElement('li');
-
-  if (jornalElement === -1) {
-    elementImg.setAttribute('src', $PhotoURL.value);
-    elementH2.textContent = $title.value;
-    elementLi2.textContent = $notes.value;
-  } else {
-    elementImg.setAttribute('src', data[jornalElement].entries.photoURL);
-    elementH2.textContent = data[jornalElement].entries.title;
-    elementLi2.textContent = data[jornalElement].entries.note;
-  }
-
-  document.querySelector('ul').appendChild(elementDiv);
-  elementDiv.appendChild(elementDiv2);
-  elementDiv2.appendChild(elementImg);
-  elementDiv.appendChild(elementDiv3);
-  elementDiv3.appendChild(elementLi);
-  elementLi.appendChild(elementH2);
-  elementDiv3.appendChild(elementLi2);
-
-}
-
-function handleSubmit(event) {
-  let index = 1;
-  event.preventDefault();
-
-  const formObject = {
-    title: $title.value,
-    photoURL: $PhotoURL.value,
-    note: $notes.value
-  };
-
-  if (data.length !== 0) {
-    index = data[data.length - 1].nextEntryId + 1;
-  }
-
-  data.push({ view: 'entry-form', entries: formObject, editing: null, nextEntryId: index });
-  index = index + 1;
-
-  newEntryDOM(-1);
-
-  $form.reset();
-  $imageElement.src = 'images/placeholder-image-square.jpg';
-  $entryForm.className = 'hidden';
-  $entries.className = 'entries-journal';
-  $noEntry.className = 'hidden';
-
-}
-
-$form.addEventListener('submit', handleSubmit);
-
 function handleBeforeUnload(event) {
   const codeJournalJSON = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', codeJournalJSON);
 }
 
 window.addEventListener('beforeunload', handleBeforeUnload);
-
-function handleDOMContentLoaded(event) {
-  for (let i = 0; i < data.length; i++) {
-    newEntryDOM(i);
-  }
-}
-
-window.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
--- js/main.js
+++ js/main.js
@@ -1,23 +1,101 @@
 /* global data */
 /* exported data */
 
-const $mainPhotoURL = document.forms[0].elements[1];
-const $mainClickNew = document.querySelector('.new-button');
-const $mainEntryForm = document.querySelector('.entry-journal');
-const $mainEntries = document.querySelector('.entries-journal');
-const $mainImageElement = document.querySelector('img');
+const $form = document.forms[0];
+const $title = document.forms[0].elements[0];
+const $PhotoURL = document.forms[0].elements[1];
+const $notes = document.forms[0].elements[2];
+
+const $clickNew = document.querySelector('.new-button');
+const $entryForm = document.querySelector('.entry-journal');
+const $entries = document.querySelector('.entries-journal');
+const $noEntry = document.querySelector('.no-entry-message');
+const $imageElement = document.querySelector('img');
+
+if (data.entries.length !== 0) {
+  $noEntry.className = 'hidden';
+}
 
 function handleInput(event) {
   const photoURL = event.target.value;
-  $mainImageElement.src = photoURL;
+  $imageElement.src = photoURL;
 }
 
-$mainPhotoURL.addEventListener('input', handleInput);
+$PhotoURL.addEventListener('input', handleInput);
 
 function handleClickNew(event) {
-  $mainEntryForm.className = 'entry';
-  $mainEntries.className = 'hidden';
+  $entryForm.className = 'entry';
+  $entries.className = 'hidden';
+  document.querySelector('ul').innerHTML = '';
+}
+
+$clickNew.addEventListener('click', handleClickNew);
+
+function newEntryDOM(jornalElement) {
+
+  const elementDiv = document.createElement('div');
+  elementDiv.setAttribute('class', 'row');
+  const elementDiv2 = document.createElement('div');
+  elementDiv2.setAttribute('class', 'column-full column-half');
+  const elementImg = document.createElement('img');
+
+  const elementDiv3 = document.createElement('div');
+  elementDiv3.setAttribute('class', 'column-half');
+  const elementLi = document.createElement('li');
+  const elementH2 = document.createElement('h2');
+
+  const elementLi2 = document.createElement('li');
+
+  elementImg.setAttribute('src', data.entries[jornalElement].photoURL);
+  elementH2.textContent = data.entries[jornalElement].title;
+  elementLi2.textContent = data.entries[jornalElement].note;
+
+  document.querySelector('ul').appendChild(elementDiv);
+  elementDiv.appendChild(elementDiv2);
+  elementDiv2.appendChild(elementImg);
+  elementDiv.appendChild(elementDiv3);
+  elementDiv3.appendChild(elementLi);
+  elementLi.appendChild(elementH2);
+  elementDiv3.appendChild(elementLi2);
 
 }
 
-$mainClickNew.addEventListener('click', handleClickNew);
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formObject = {
+    title: $title.value,
+    photoURL: $PhotoURL.value,
+    note: $notes.value,
+    entryId: data.nextEntryId
+
+  };
+
+  data.entries.unshift(formObject);
+  data.nextEntryId = data.nextEntryId + 1;
+  $imageElement.src = 'images/placeholder-image-square.jpg';
+  $entryForm.className = 'hidden';
+  $entries.className = 'entries-journal';
+  $noEntry.className = 'hidden';
+
+  $form.reset();
+
+  if (data.entries.length === 1) {
+    newEntryDOM(0);
+  } else {
+    for (let i = 0; i < data.entries.length; i++) {
+      newEntryDOM(i);
+    }
+  }
+
+}
+
+$form.addEventListener('submit', handleSubmit);
+
+function handleDOMContentLoaded(event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    newEntryDOM(i);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
