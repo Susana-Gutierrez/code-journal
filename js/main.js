fd6ddd4d3f304723a7f87e1225934b056a84ac36
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2021-08-28 18:21:07 -0700

    Completed edit-an-entry.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ const $entries = document.querySelector('.entries-journal');
 const $noEntry = document.querySelector('.no-entry-message');
 const $imageElement = document.querySelector('img');
 
+const $taskList = document.querySelector('.task-list');
+
 if (data.entries.length !== 0) {
   $noEntry.className = 'hidden';
 }
@@ -19,6 +21,9 @@ if (data.entries.length !== 0) {
 function handleInput(event) {
   const photoURL = event.target.value;
   $imageElement.src = photoURL;
+  if (photoURL === '') {
+    $imageElement.src = 'images/placeholder-image-square.jpg';
+  }
 }
 
 $PhotoURL.addEventListener('input', handleInput);
@@ -44,6 +49,13 @@ function newEntryDOM(jornalElement) {
   const elementLi = document.createElement('li');
   const elementH2 = document.createElement('h2');
 
+  const elementSpan = document.createElement('span');
+  elementSpan.setAttribute('class', 'pencil');
+
+  const elementI = document.createElement('i');
+  elementI.setAttribute('class', 'fas fa-pen');
+  elementI.setAttribute('data-entry-id', data.entries[jornalElement].entryId);
+
   const elementLi2 = document.createElement('li');
 
   elementImg.setAttribute('src', data.entries[jornalElement].photoURL);
@@ -51,28 +63,54 @@ function newEntryDOM(jornalElement) {
   elementLi2.textContent = data.entries[jornalElement].note;
 
   document.querySelector('ul').appendChild(elementDiv);
+
   elementDiv.appendChild(elementDiv2);
   elementDiv2.appendChild(elementImg);
   elementDiv.appendChild(elementDiv3);
   elementDiv3.appendChild(elementLi);
   elementLi.appendChild(elementH2);
+  elementH2.appendChild(elementSpan);
+  elementSpan.appendChild(elementI);
+
   elementDiv3.appendChild(elementLi2);
 
 }
 
 function handleSubmit(event) {
   event.preventDefault();
+  var photo = '';
 
-  const formObject = {
-    title: $title.value,
-    photoURL: $PhotoURL.value,
-    note: $notes.value,
-    entryId: data.nextEntryId
+  if ($PhotoURL.value === '') {
+    photo = 'images/placeholder-image-square.jpg';
+  } else {
+    photo = $PhotoURL.value;
+  }
 
-  };
+  if (data.editing !== null) {
+    var index = data.editing.entryId;
+    for (var i = 0; i < data.entries.length; i++) {
+      if (data.entries[i].entryId === index) {
+        data.entries[i].title = $title.value;
+        data.entries[i].photoURL = photo;
+        data.entries[i].note = $notes.value;
+      }
+    }
 
-  data.entries.unshift(formObject);
-  data.nextEntryId = data.nextEntryId + 1;
+  } else {
+
+    const formObject = {
+      title: $title.value,
+      photoURL: photo,
+      note: $notes.value,
+      entryId: data.nextEntryId
+
+    };
+
+    data.entries.unshift(formObject);
+    data.nextEntryId = data.nextEntryId + 1;
+
+  }
+
   $imageElement.src = 'images/placeholder-image-square.jpg';
   $entryForm.className = 'hidden';
   $entries.className = 'entries-journal';
@@ -87,7 +125,7 @@ function handleSubmit(event) {
       newEntryDOM(i);
     }
   }
-
+  data.editing = null;
 }
 
 $form.addEventListener('submit', handleSubmit);
@@ -99,3 +137,28 @@ function handleDOMContentLoaded(event) {
 }
 
 window.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
+
+function handleClickEntry(event) {
+
+  if (event.target.tagName === 'I') {
+    var listEntryId = event.target.getAttribute('data-entry-id');
+    for (var i = 0; i < data.entries.length; i++) {
+      if (data.entries[i].entryId === parseInt(listEntryId)) {
+        data.editing = data.entries[i];
+        $title.value = data.entries[i].title;
+        if (data.entries[i].photoURL === 'images/placeholder-image-square.jpg') {
+          $PhotoURL.value = '';
+        } else {
+          $PhotoURL.value = data.entries[i].photoURL;
+        }
+        $notes.value = data.entries[i].note;
+        $imageElement.src = data.entries[i].photoURL;
+      }
+    }
+    $entryForm.className = 'entry';
+    $entries.className = 'hidden';
+    document.querySelector('ul').innerHTML = '';
+  }
+}
+
+$taskList.addEventListener('click', handleClickEntry);
